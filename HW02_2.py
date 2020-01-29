ce038ce6ba8cc90cf6de718f9375b83284f37a45
maximskakodub <60297935+maximskakodub@users.noreply.github.com>
2020-01-29 22:24:33 +0200

    Add files via upload

--- HW02_2.py
+++ HW02_2.py
@@ -1,18 +1,18 @@
-from random import randrange as rand
-a=rand(0,10)
-b=rand(0,10)
-c=rand(0,10)
-if a > b:
-    print("Неплохо!")
-elif a < b:
-    print("Плохо!")
-elif a == b:
-   print ("Это ещё не всё")
-   print(b,a+b)
-if (a + b) < b:
-        print("Великолепно!!!")
-    elif (a + b) > b:
-        print("Ужасно!!!")
-    elif (a + b) == b:
-        print("Всё пропало!")
+import random
+a=(random.randint(0,10))
+b=(random.randint(0,10))
+c=(random.randint(0,10))
+print(a,b,c)
 
+if a>b:
+    print("Неплохо!")
+if a<b:
+    print("Плохо!")
+if a==b:
+    print("Это ещё не всё")
+    if (a+b)<c:
+        print("Великолепно!!!")
+    if (a+b)>c:
+        print("Ужасно!!!")
+    if (a+b)==c:
+        print("Всё Пропало!!!")
