945cee8b904595fed62b73a1c9159852c576524a
maximskakodub <60297935+maximskakodub@users.noreply.github.com>
2020-02-15 07:43:31 +0200

    Add files via upload

--- HW03_2.py
+++ HW03_2.py
@@ -1,12 +1,12 @@
 from random import randint as rand
 
-a = (int(input( "Введіть бажану довжину списку ")))
-b = (int(input("Введіть бажане максимальне значення елементів списку ")))
+a = (int(input( "Довжину списку ")))
+b = (int(input("Максимальне значення елементів списку ")))
 
-def spisok (x,y):
+def spisok (c,d):
     list = []
-    for i in range(x):
-        list.append(rand(0,y+1))
+    for i in range(c):
+        list.append(rand(0,d+1))
     return list
 
 print(spisok(a,b))
